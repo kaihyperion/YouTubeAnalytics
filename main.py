f1a1639a17b60b29589797cb6d9dc253b7878a0b
kaihyperion <kyperion.workmode@gmail.com>
2022-01-30 18:05:07 -0600

    Finished merging creating and retrieving report jobs

--- main.py
+++ main.py
@@ -1,18 +1,135 @@
 import os
 import json
+import argparse
+import oauth
+import requests
+from googleapiclient.discovery import build
+import oauth2client.client
+import tabulate
+import numpy as np
+import pandas as pd
+from io import FileIO
+from googleapiclient.http import MediaIoBaseDownload
+import time
+import errno
+def reportCreate(YT):
+    print("These are the available Report Types Available to create:\n")
+    reportTypeLists = YT.reportTypes().list().execute()
 
+    df = pd.DataFrame(reportTypeLists['reportTypes'])
+    print(df)
+    print('\n')
+    usr_input = input("Please type which specific report types (ID) you would like to create:\n(A) if you would like to Select ALL \nmake sure they are spaced: channel_annotations_a1 channel_basic_a2\nenter here: ")
+    if usr_input == 'A':
+        if 'reportTypes' in reportTypeLists and reportTypeLists['reportTypes']:
+            reportTypes = reportTypeLists['reportTypes']
+            for report in reportTypes:
+                reporting_job = YT.jobs().create(body=dict(reportTypeId = report['id'], name = report['name'])).execute()
+                print ('Reporting job created for reporting type "%s" at "%s"'
+                       % ( reporting_job['reportTypeId'],
+                          reporting_job['createTime']))
 
+                
+    print("Finished Creating jobs\n")
 
-def main(**kwargs):
-    for kw in kwargs:
-        print(kw, '-', kwargs[kw])
+def getJobList(YT):
+    print("These are the currently created Reporting jobs available:\n")
+    results = YT.jobs().list().execute()
+    if 'jobs' in results and results['jobs']:
+        jobs = results['jobs']
+        idList = []
+        for job in jobs:
+            print('Reporting job id: %s\n name: %s\n for reporting type: %s\n'
+                % (job['id'], job['name'], job['reportTypeId']))
+        idList.append([job['id'],job['reportTypeId']])
+    return idList
 
+def retrieve_reports(YT, **kwargs):
+    results = YT.jobs().reports().list(**kwargs).execute()
+    if 'reports' in results and results['reports']:
+        reports = results['reports']
+        for report in reports:
+            print ('Report dates: %s to %s\n       download URL: %s\n'
+                % (report['startTime'], report['endTime'], report['downloadUrl']))
+        return reports
+    else:
+      print("no reports found")
 
-if __name__ == "__main__":
-    main(a = 24, b = 87, c =3, d =46)
+def download_report(YT, report_url, reportTypeName, timeList):
+    request = YT.media().download(
+    resourceName=' '
+    )
+    for i in range(len(report_url)):
+        request.uri = report_url[i]
+        fileName = 'data/'+reportTypeName+'/'+timeList[i]+'.csv'
+        if not os.path.exists(os.path.dirname(fileName)):
+            try:
+                os.makedirs(os.path.dirname(fileName))
+            except OSError as exc:
+                if exc.errno != errno.EEXist:
+                    raise
+            
+        fh = FileIO(fileName, mode= 'wb')
+        downloader = MediaIoBaseDownload(fh, request, chunksize=-1)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            if status:
+                print ('Download %d%%.' % int(status.progress() * 100))
+        print ('Download Complete!')
+
+    time.sleep(20)
     
-
 #kwargs is a pararmeter name and acts liek a dictioanry
 # kwargs is like keyword (named) arguemnts. so key and value. must have '=' equla sign
 # ** is the 'unpacking operator' it unpacks the argument passed as dictionary
 # *args is positional argument
+
+
+
+#Create a reporting job
+
+
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser()
+    
+    # parser.add_argument('--content-owner', default= '',
+    #                     help = 'ID of content owner for which you are retrieving jobs and reports.')
+    # parser.add_argument('--include-system-managed', default = False,
+    #                     help = 'Whether the API response should include system-managed reports')
+    # parser.add_argument('--name', default = '',
+    #                     help= 'Name for the reporting job. The script prompts you to set a name ' +
+    #                     'for the job if you do not provide one using this argument')
+    # parser.add_argument('--report-type', default=None,
+    #                     help = 'The type of report for which you are creating a job')
+    # args = parser.parse_args()
+    
+    scope = ['https://www.googleapis.com/auth/yt-analytics.readonly', 'https://www.googleapis.com/auth/yt-analytics-monetary.readonly']
+    SCOPE = ''
+    for i in scope:
+        SCOPE += i+' '
+        
+    auth = oauth.Authorize(scope = SCOPE, token_file = 'token.yaml', secrets_file = 'secret_kai.json')
+    auth.authorize()
+    token = auth.load_token()
+    credentials = auth.get_credentials()
+    
+    serviceName = 'youtubereporting'
+    API_VERSION = 'v1'
+    youtubeReporting = build(serviceName= serviceName, version= API_VERSION,
+              credentials= credentials)
+    
+    print("Welcome to AMA's YouTube Reporting API Data Collector! \n")
+    usr_input = input("Would you like to '(C)reate' or '(R)etrieve' or (L)ist jobs?  ")
+    if usr_input == 'C':
+        reportCreate(youtubeReporting)
+    if usr_input == 'L':
+        joblist = getJobList(youtubeReporting)
+        ask = input("Would you like to retrieve these jobs?: Y/N")
+        if ask == 'Y':
+            for i in joblist:
+                reports = retrieve_reports(youtubeReporting, jobId = i[0],onBehalfOfContentOwner='')
+                url_list = [i['downloadUrl'] for i in reports]
+                t_list = [i['endTime'] for i in reports]
+                download_report(youtubeReporting, url_list, id[1], t_list)
--- oauth.py
+++ oauth.py
@@ -5,13 +5,17 @@ from requests.adapters import HTTPAdapter
 from requests_oauthlib import OAuth2Session #https://docs.authlib.org/en/latest/client/oauth2.html
 from urllib3.util.retry import Retry
 from yaml import load, dump, YAMLError
-
+from oauth2client.client import OAuth2Credentials
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
     
-    
+
+'''
+https://developers.google.com/identity/protocols/oauth2/web-server#httprest_3
+
+'''
 # Google's OAUTH 2.0 endpoint url
 authorization_base_url = 'https://accounts.google.com/o/oauth2/v2/auth'
 token_uri = 'https://www.googleapis.com/oauth2/v4/token'
@@ -22,6 +26,10 @@ class Authorize:
         self.token_file = token_file
         self.session = None
         self.token = None
+        
+        self.access_token = None
+        self.refresh_token = None
+        self.token_expiry = None
         
         try:
             with open(secrets_file, 'r') as stream:
@@ -43,18 +51,44 @@ class Authorize:
         try:
             with open(self.token_file, 'r') as stream:
                 token = load(stream, Loader=Loader)
+                self.access_token = token['access_token']
+                self.redirect_token = token['refresh_token']
+                self.token_expiry = token['expires_at']
+                
         except (YAMLError, IOError):
             return None
         return token
     
     def save_token(self, token):
+        
         with open(self.token_file, 'w') as stream:
             dump(token, stream, Dumper = Dumper)
         os.chmod(self.token_file, 0o600)
+    def re_authorize(self):
+        print(self.scope)
+        self.session = OAuth2Session(self.client_id, scope=self.scope,
+                                        redirect_uri = self.redirect_uri,
+                                        auto_refresh_url = self.token_uri,
+                                        auto_refresh_kwargs = self.extra,
+                                        token_updater=self.save_token)
+        
+        authorization_url, _ = self.session.authorization_url(
+            authorization_base_url,
+            access_type="offline",
+            prompt="select_account")
+        print("Please click the link and authorize: ", authorization_url)
+        
+        response_code = input('Paste the response token: ')
+        
+        self.token = self.session.fetch_token(
+            self.token_uri, client_secret = self.client_secret,
+            code = response_code)
+        self.save_token(self.token)
         
     def authorize(self):
         token = self.load_token()
         
+     
         if token:
             self.session = OAuth2Session(self.client_id, token= token,
                                          auto_refresh_url = self.token_uri,
@@ -89,7 +123,27 @@ class Authorize:
                         method_whitelist = frozenset(['GET', 'POST']),
                         raise_on_status = False)
         self.session.mount('https://', HTTPAdapter(max_retries=retries))
+    
+    def get_credentials(self):
+         cred = OAuth2Credentials(access_token= self.access_token,
+                           client_id = self.client_id,
+                           client_secret= self.client_secret,
+                           refresh_token= self.refresh_token,
+                           token_expiry= self.token_expiry,
+                           token_uri=self.token_uri,
+                           user_agent = '')
+         
+         return cred
+
+# a = Authorize(scope ='https://www.googleapis.com/auth/yt-analytics.readonly https://www.googleapis.com/auth/yt-analytics-monetary.readonly',token_file="token.yaml", secrets_file='secret_kai.json')
+# a.authorize()
 
 
-a = Authorize(scope ='https://www.googleapis.com/auth/yt-analytics.readonly https://www.googleapis.com/auth/yt-analytics-monetary.readonly',token_file="token.yaml", secrets_file='secret_kai.json')
-a.authorize()
+'''
+in the token yaml file
+access_token: the token that your application sends to authorize a Google API request
+expires_in:  The remaining lifetime of the access token in secondsrefresh_token: A token tha tyou can use to obtain a new access token., Refresh tokens are valid until the 
+              user revokes access. Again, this field is only present in this response if you set the access_type 
+                parameter to offline in the initial request to Google's authorization server
+scope: the scopes of access granted by the access_token expressed as a list of space limited, case-sensitive strings
+token_type: the type of token returned. At this time, this field's value is always set to Bearer'''
